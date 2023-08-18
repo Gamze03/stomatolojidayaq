e82707590257138154694062d8bfe444076e5249
qemze <qemzemirkisiyeva90@gmail.com>
2023-08-18 16:51:30 +0400

    stom-dayaq layihesi bitdi

--- swiper.js
+++ swiper.js
@@ -1,10 +1,10 @@
 const bannerSwiper = new Swiper(".banner-swiper", {
     slidesPerView: 1,
     speed: 1000,
-    // loop: true,
-    // autoplay: {
-    //     delay: 2000,
-    // },
+    loop: true,
+    autoplay: {
+        delay: 2000,
+    },
     pagination: {
         el: ".swiper-pagination",
         clickable: true,
